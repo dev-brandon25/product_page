c29361016f6159f2ab9cb7aa808511e82de35f9a
brandon <bmgibson22@gmail.com>
2025-03-28 00:21:33 -0400

    Navbar built

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
-import { Button, useColorMode } from "@chakra-ui/react";
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import CreatePage from "./Pages/CreatePage";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const { colorMode, toggleColorMode } = useColorMode();
-
   return (
-    <Button onClick={toggleColorMode}>
-      Toggle {colorMode === "light" ? "Dark" : "Light"}
-    </Button>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { ChakraProvider } from "@chakra-ui/react";
 import customTheme from "./theme"; // Import the custom theme
+import {BrowserRouter} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider theme={customTheme}>
-      <App />
-    </ChakraProvider> 
+    <BrowserRouter>
+      <ChakraProvider theme={customTheme}>
+        <App />
+      </ChakraProvider> 
+    </BrowserRouter>
   </StrictMode>,
 )
