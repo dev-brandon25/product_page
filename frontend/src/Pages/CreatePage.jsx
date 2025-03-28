5a86532c925356d7bf5967b012f6c6b1592c4b11
brandon <bmgibson22@gmail.com>
2025-03-28 14:21:00 -0400

    Heading & Input add to createPage

--- frontend/src/Pages/CreatePage.jsx
+++ frontend/src/Pages/CreatePage.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useState } from "react";
+import {
+  Container,
+  VStack,
+  Heading,
+  Box,
+  useColorModeValue,
+  Input,
+} from "@chakra-ui/react";
 
 function CreatePage() {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
   return (
-    <div>CreatePage</div>
-  )
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+        </Heading>
+        <Box
+          w={"full"}
+          bg={useColorModeValue("White", "gray.800")}
+          padding={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input placeholder="Product name" />
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
 }
 
-export default CreatePage
+export default CreatePage;
